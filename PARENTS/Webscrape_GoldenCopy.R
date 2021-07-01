51ef080b29e407a972dc29f1c6681c46cd969e2f
oalsihvtearri <oliverashtari@MacBook-Air-di-Oliver.local>
2021-07-01 23:06:21 +0100

    Added check if file exists next

--- PARENTS/Webscrape_GoldenCopy.R
+++ PARENTS/Webscrape_GoldenCopy.R
@@ -1,3 +1,14 @@
+###################################################
+######  Golden Copy Webscrape               ######
+######  Oliver Ashtari Tafti                ######
+######  Created 24 June 2021                ######
+###################################################
+######  INSTRUCTIONS                          #####
+######                                        #####
+######                                        #####
+###################################################
+
+## Load libraries
 library("httr")
 library("jsonlite")
 library("XML")
@@ -6,21 +17,24 @@ library("sjmisc")
 library("stringr")
 library("dplyr")
 
+## Set working directory
 setwd("/Users/oliverashtari/desktop/Liliana/PARENTS/RAW")
 
-## Create end of month dates, from 2017 (when data starts) to today BY 6 MONTHS 
+## Create end of month dates, from 2018-02 (when data starts) to today
 # note Concatednated available from May 2017
-eom <-  format(seq(as.Date("2018-03-01"), as.Date("2021-01-01"), by="1 month") - 1,"%Y%m%d")
+eom <-  format(seq(as.Date("2018-03-01"), 
+                   as.Date("2021-01-01"), by="7 day") - 1,"%Y%m%d")
 
-## Create url links
+## Create URLs
 url_first <- "https://goldencopy.gleif.org/api/v2/golden-copies/publishes/rr/"
 url_all   <- c(gsub(" ", "", paste(url_first, eom, "-0000.csv")))
 
+## Loop over all URLs
 for (u in url_all) {
-
+                    if (file.exists(basename(u))) {
+                      next
+                    }
+  
         download.file(u, basename(u), mode = "wb")
         unzip(basename(u))
-  
-  
 }
-
